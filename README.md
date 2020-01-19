c337bddc3d05740f9e7398268da61547f8e7ff1c
han1997 <hanhuiyuan1997@gmail.com>
2020-01-19 21:18:33 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 学习码匠社区
+
+# 资料
+- [课程网址](https://www.bilibili.com/video/av50200264)
+- [Spring文档](https://spring.io/guides)
+- [Bootstrap](https://v3.bootcss.com)
+- [Github Oauth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+- [查找maven_jar包](https://search.maven.org/artifact/)
---
+++ src/main/java/com/hhy/community/community/provider/GithubProvider.java
@@ -0,0 +1,48 @@
+package com.hhy.community.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.hhy.community.community.dto.AccessTokenDTO;
+import com.hhy.community.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+
+/**
+ * @author handaxingyuner
+ */
+
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(mediaType,JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        } catch (IOException e) {
+            System.out.println("GithubProvider-Post出错！");
+        }
+        return null;
+    }
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            System.out.println("getUser 出错！");
+        }
+        return null;
+    }
+}
